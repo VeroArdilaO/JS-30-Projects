bf7da16f6157cf7b352c4c27f44d400ec887fabf
Veronica <veronica.ardilao@outlook.es>
2023-03-01 15:33:45 -0500

    Adding task 11, custom video player

---
+++ 11.Custom-video-player/index.js
@@ -0,0 +1,55 @@
+//Get all the elements
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector(".progress");
+const progressBar = document.querySelector(".progress__filled");
+const toggle = document.querySelector(".toggle");
+const skipButtons = document.querySelectorAll('[data-skip]');
+const ranges = document.querySelectorAll(".player__slider");
+
+//Build out functions
+
+function togglePlay(){
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip(){
+    console.log(this.dataset.skip)
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+function handleRangeUpdate(){
+    video[this.name] = this.value;
+    console.log(this.name);
+    console.log(this.value);
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+  }
+
+  function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth)* video.duration;
+    video.currentTime = scrubTime;
+  }
+//hook up add event listeners
+let mousedown = false;
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
+
